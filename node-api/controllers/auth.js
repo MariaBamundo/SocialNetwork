9a39819f0518e8249ed0ecf20cd02daa882702e9
Maria <mariarose.bamundo@clcs.conduent.com>
2020-10-19 18:07:24 -0400

    lesson 44

--- node-api/controllers/auth.js
+++ node-api/controllers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const User = require("../models/user");
 
 exports.signup = async (req, res) => {
@@ -6,4 +8,30 @@ exports.signup = async (req, res) => {
     const user = await new User(req.body);
     await user.save();
     res.status(200).json({ message: "Signup succes! Please login." });
+}
+
+exports.signin = (req, res) => {
+    // find user by email
+    const {email, password} = req.body
+    User.findOne({email}, (error, user) => {
+        if(error || !user) {
+            return res.status(401).json({
+                error: "User with that email does not exist."
+            });
+        }
+        // match email and password
+        if(!userSchema.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        }
+
+        // generate a token with user id and secret
+        const token = jwt.sign({_id: user.id}, process.env.JWT_SECREt);
+        // presist the token as 't' in cookie with expiry date
+        res.cookie("t", token, {expire: new Date() + 9999});
+        // return response with user and token to front end client
+        const {_id, name , email} = user;
+        return res.json({token, user: {_id, name , email} });
+    });
 }
--- node-api/validator/index.js
+++ node-api/validator/index.js
@@ -1,10 +1,10 @@
 exports.createPostValidator = (req, res, next) => {
     // title
     req.check('title', "Write a title").notEmpty();
-    req.check('title', "Title must 4-150 characters").isLength({ min: 4, max: 150 });
+    req.check('title', "Title must be 4-150 characters").isLength({ min: 4, max: 150 });
     // body
     req.check('body', "Write a body").notEmpty();
-    req.check('body', "body must 4-2000 characters").isLength({ min: 4, max: 2000 });
+    req.check('body', "body must be 4-2000 characters").isLength({ min: 4, max: 2000 });
     // check for errors
     const errors = req.validationErrors();
     // if error show first on as they happen
@@ -15,3 +15,33 @@ exports.createPostValidator = (req, res, next) => {
     // proceed to next middleware
     next();
 };
+
+exports.userSignupValidator = (req, res, next) => {
+    req.check('name', "Name is required")
+        .notEmpty()
+        .isLength({min: 4, mac: 10})
+        .withMessage("Name must be 4-10 characters");
+
+    req.check('email')
+        .matches(/.+\@.+\..+/)
+        .withMessage("Invalid email address")
+        .isLength({ min: 3, max: 32 })
+        .withMessage("Email must be 3-32 characters");
+
+    req.check('password', "Password is required").notEmpty();
+    req.check('password')
+        .isLength({min: 6})
+        .withMessage("Password must be at least 6 characters")
+        .matches(/\d/)
+        .withMessage("Password must contain at least one number");
+
+    // check for errors
+    const errors = req.validationErrors();
+    // if error show first on as they happen
+    if(errors) {
+        const firstError = errors.map((error) => error.msg)[0];
+        return res.status(400).json({error: firstError});
+    }
+    // proceed to next middleware
+    next();
+}
